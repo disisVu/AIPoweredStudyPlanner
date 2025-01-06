ecf81e36d3cac6f3c5f124e9a01ec5e96734e20b
disisVu <102909483+disisVu@users.noreply.github.com>
2025-01-06 20:53:00 +0700

    Update api.ts

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosPrivate = axios.create({
-  baseURL: 'https://aipoweredstudyplanner.onrender.com',
+  baseURL: 'https://localhost:5000',
   headers: {
     'Content-Type': 'application/json'
   }
